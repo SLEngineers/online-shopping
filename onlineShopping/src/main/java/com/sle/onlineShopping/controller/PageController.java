c2d711f7d18bb065b7fa4139fcefe5d0e7c4e7d9
Vishal Kumar <bikky95vishal@gmail.com>
2021-01-05 22:26:25 +0530

    successfully loaded static resources

--- onlineShopping/src/main/java/com/sle/onlineShopping/controller/PageController.java
+++ onlineShopping/src/main/java/com/sle/onlineShopping/controller/PageController.java
@@ -4,7 +4,6 @@
 package com.sle.onlineShopping.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -33,7 +32,7 @@ public class PageController {
 		ModelAndView mv = new ModelAndView("page");
 		mv.addObject("greeting", greeting);
 		return mv;
-	}*/
+	}
 	
 	@RequestMapping(value = {"/test/{greeting}"})
 	public ModelAndView test(@PathVariable(value = "greeting") String greeting){
@@ -43,5 +42,6 @@ public class PageController {
 		ModelAndView mv = new ModelAndView("page");
 		mv.addObject("greeting", greeting);
 		return mv;
-	}
+	}*/
+	
 }
