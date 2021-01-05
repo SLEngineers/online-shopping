d3e018b122f1b97c810dcbe71f3260b871d5e937
Vishal Kumar <bikky95vishal@gmail.com>
2021-01-06 00:21:30 +0530

    master page creation done

--- onlineShopping/src/main/java/com/sle/onlineShopping/controller/PageController.java
+++ onlineShopping/src/main/java/com/sle/onlineShopping/controller/PageController.java
@@ -18,10 +18,37 @@ public class PageController {
 	@RequestMapping(value = {"/", "/home", "/index"})
 	public ModelAndView index(){	
 		ModelAndView mv = new ModelAndView("page");
-		mv.addObject("greeting", "Welcome!");
+		mv.addObject("userClicksHome", true);
+		mv.addObject("title", "Home");
 		
 		return mv;
 	}
+	
+	@RequestMapping(value = "/about")
+	public ModelAndView about(){	
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("userClicksAbout", true);
+		mv.addObject("title", "About Us");
+		return mv;
+	}
+	
+	@RequestMapping(value = "/contact")
+	public ModelAndView contact(){	
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("userClicksContact", true);
+		mv.addObject("title", "Contact Us");
+		return mv;
+	}
+	
+	@RequestMapping(value = "/listProducts")
+	public ModelAndView ListProducts(){	
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("userClicksListProducts", true);
+		mv.addObject("title", "Our Products");
+		return mv;
+	}
+	
+	
 
 	
 	/*@RequestMapping(value = {"/test"})
